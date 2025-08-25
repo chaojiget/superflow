1d22293588976427dfb035d3213c5de2fd1d0588
chaojiget <jianyongl365@gmail.com>
2025-08-25 13:52:16 +0800

    fix: resolve App import conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
+import { useRef, useState } from 'react';
 import { copyText } from '@/utils';
 
 const App: React.FC = () => {
