238a346049294c9eb453049f2b08de1a8cb88cb6
chaojiget <jianyongl365@gmail.com>
2025-09-04 15:09:52 +0800

    fix: 避免 logger 适配器 lint 错误

--- packages/@app/services/src/adapters/logger.ts
+++ packages/@app/services/src/adapters/logger.ts
@@ -2,9 +2,11 @@ import type { LogPort } from '../ports/log';
 
 export const loggerAdapter: LogPort = {
   info(message) {
+    // eslint-disable-next-line no-console
     console.log(message);
   },
   error(message) {
+    // eslint-disable-next-line no-console
     console.error(message);
   },
 };
