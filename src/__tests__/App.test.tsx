9bb3e363524c01ea8d189833631a31d7d3fb1f5c
chaojiget <jianyongl365@gmail.com>
2025-09-07 14:30:30 +0800

    chore: 添加文档构建与渲染测试

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('App', () => {
+  it('页面能够渲染', () => {
+    render(<App />);
+    expect(screen.getByText('工作流编排 Studio').outerHTML).toMatchSnapshot();
+  });
+});
